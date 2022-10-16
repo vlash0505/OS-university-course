05ce06cdcdb4a5dbabf9646402d0735686f7e563
vlash0505 <shupliakovvl@gmail.com>
2022-10-17 00:26:47 +0300

    [Lab1] fixed the input

--- Lab1/src/main/java/EntryPoint.java
+++ Lab1/src/main/java/EntryPoint.java
@@ -5,12 +5,12 @@ public class EntryPoint {
     public static void main(String[] args) {
         //as the function takes only one parameter,
         //we should handle that case accordingly
-//        if (args.length != 1) {
-//            return;
-//        }
+        if (args.length != 1) {
+            return;
+        }
 
         try {
-            new Manager().initManager(Integer.parseInt("0"));
+            new Manager().initManager(Integer.parseInt(args[0]));
         } catch (IOException e) {
             e.printStackTrace();
         }
